0b027c7a961a2535133e8e37c88aee602eb29252
Glitch (server-aventuras) <none>
2020-06-25 05:35:06 +0000

    Rewound to commit a9554b78df930f75e8bab7604a208afe6785fef6 + 0 edits

--- server.js
+++ server.js
@@ -1,43 +1,34 @@
-// Este código hace que funcione Glitch
-const http = require('http');                                    
-const express = require('express');                              
-const app = express();                                           
-app.use(express.static('public'));                               
-app.get("/", function (request, response) {                      
-  response.sendFile(__dirname + '/views/index.html');            
-});                                                              
-app.get("/", (request, response) => {                            
-  response.sendStatus(200);                                      
-});                                                              
-app.listen(process.env.PORT);                                    
-setInterval(() => {                                              
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);   
-}, 280000);                                                      
+// server.js
+// where your node app starts
 
-// Definimos las variables necesarias para que el bot funcione
-const Discord = require("discord.js");
-const client = new Discord.Client();
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require("express");
+const app = express();
 
-// Definimos el prefix del bot
-let prefix = "-"
+// our default array of dreams
+const dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
 
-// Activamos el evento ready
-client.on('ready', () => {
-  console.log('El bot ha sido encendido con éxito.')
-})
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
 
-// Activamos el evento message
-client.on('message', async (message) => {
-  
-  // Hacemos que otros bots no puedan usar los comandos de este bot
-  if(message.author.bot) return;
-  
-  // Creamos el comando ping
-  if(message.content.startsWith(prefix + 'Ip')) {
-    message.channel.send('mc.aventuras-server.ga')
-  }
-  
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
 });
 
-// Iniciamos sesión en Discord
-client.login(process.env.TOKEN)
+// send the default array of dreams to the webpage
+app.get("/dreams", (request, response) => {
+  // express helps us take JS objects and send them as JSON
+  response.json(dreams);
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
