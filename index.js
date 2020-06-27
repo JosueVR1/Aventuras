3214b57d992ed3801318cfcc4c55de98e193808e
Glitch (server-aventuras) <none>
2020-06-27 15:56:11 +0000

    😇🌚 Checkpoint ./index.js:18097154/2013 ./watch.json:18097154/2

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
-// Este código hace que funcione Glitch
-const http = require('http');                                    
-const express = require('express');                              
-const app = express();                                           
-app.use(express.static('public'));                               
-app.get("/", function (request, response) {                      
-  response.sendFile(__dirname + '/views/index.html');            
-});                                                              
-app.get("/", (request, response) => {                            
-  response.sendStatus(200);                                      
-});                                                              
-app.listen(process.env.PORT);                                    
-setInterval(() => {                                              
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);   
-}, 280000);                                                      
+//ESTE CODIGO NO AFECTARA SU BOT, SCRIPT DE ARRANQUE
 
-// Definimos las variables necesarias para que el bot funcione
-const Discord = require("discord.js");
+const http = require('http');
+const express = require('express');
+const app = express();
+
+//
+app.use(express.static('public'));
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+
+app.listen(process.env.PORT);
+
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`); 
+}, 280000);
+
+
+//DESDE AQUI EMPIEZA A ESCRIBIR EL CODIGO PARA SU BOT
+
+const Discord = require('discord.js');
 const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('estoy listo!');
+  client.user.setGame(`prueba help`);
+});
+
+client.on('message', message => {
+  if (message.content.startsWith("ping")) {
+   let ping = Math.floor(message.client.ping);
+   message.channel.send(':ping_pong: `'+ping+' ms.`'); 
+  }
+  
+});
 
 // Definimos el prefix del bot
 let prefix = "-"
